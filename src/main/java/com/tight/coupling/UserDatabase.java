b6a4c0b0150a915869964602519e25ec1dfa3b94
Pranay Jain <jainpranay20@gmail.com>
2024-10-05 21:59:12 +0530

    Initial commit

---
+++ src/main/java/com/tight/coupling/UserDatabase.java
@@ -0,0 +1,10 @@
+package com.tight.coupling;
+
+
+// Let's added all the code use to interact with the db
+public class UserDatabase {
+
+    public String getUserDetails() {
+        return "User Details From Database";
+    }
+}
