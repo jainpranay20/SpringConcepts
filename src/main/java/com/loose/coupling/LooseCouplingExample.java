1bf025c9917a3281a59bdf0cf0602b1d96bb168f
Pranay Jain <jainpranay20@gmail.com>
2024-10-06 13:14:40 +0530

    Added one more method for loose coupling example

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -4,6 +4,11 @@ public class LooseCouplingExample {
     public static void main(String[] args) {
          UserDataProvider dataProvider = new UserDatabaseProvider();
          UserManager userManager = new UserManager(dataProvider);
-        System.out.println(userManager.getUserInfo());
+         System.out.println(userManager.getUserInfo());
+
+         // we can create a reference variable of an interface and assign to an object absolutely valid in java
+         UserDataProvider webServiceDataProvider = new WebServiceDataProvider();
+         UserManager userManager1 = new UserManager(webServiceDataProvider);
+         System.out.println(userManager1.getUserInfo());
     }
 }
